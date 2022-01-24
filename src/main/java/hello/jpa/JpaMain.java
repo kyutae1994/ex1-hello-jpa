926969b45546313ec50fdf33af2a7baa9ae5c61f
kyutae1994 <k305059ute@gmail.com>
2022-01-24 17:28:49 +0900

    JPA ORM 처음세팅 및 공부

---
+++ src/main/java/hello/jpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hello.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // 어플리케이션 로딩 시점에 딱 하나만 만들어야됨
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 예를 들어 행위를 하고 나가는 경우 db 커넥션을 얻어서 쿼리 날리고 종료할때마다
+        // 데이터베이스 커넥션 하나 받았다고 생각하면 된다.
+        // 쓰레드간에 공유 X (사용하고 버려야 한다)
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
